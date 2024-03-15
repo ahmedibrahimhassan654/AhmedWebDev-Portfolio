b18f39bbf3001ac275da36866aee2cd69c4d722d
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-03-15 13:13:30 +0200

    edit about page

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -28,39 +28,57 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
+        <h5 className="purple">Type-Script</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <RiReactjsFill />
+        <h5 className="purple">React Js </h5> 
+
       </Col>
   
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <h5 className="purple">java Script</h5> 
+
       </Col>
   
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <h5 className="purple">Node Js </h5> 
+
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
+        <h5 className="purple">Redux</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <h5 className="purple">Mongo DB</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
+        <h5 className="purple">Next Js </h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <h5 className="purple">Git</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
+        <h5 className="purple">Firebase</h5> 
+
       </Col>
    
       <Col xs={4} md={2} className="tech-icons">
         <FaWordpress  />
+        <h5 className="purple">Wordpress</h5> 
+
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,25 +6,38 @@ import {
   SiSlack,
   SiVercel,
   SiMacos,
+  SiCanva 
 } from "react-icons/si";
+import { TbBrandRedux  } from "react-icons/tb";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
+   
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <h5 className="purple">Visual studio code</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <h5 className="purple">post man </h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
+        <h5 className="purple">Chat GPT</h5> 
+
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiCanva />
+        <h5 className="purple">Canva</h5> 
+
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandRedux />
+        <h5 className="purple">Redux dev tools</h5> 
+
       </Col>
     </Row>
   );
