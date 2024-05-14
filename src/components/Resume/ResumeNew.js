a5fd9c3145f36154b987a5fc79f60d90039ba108
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-05-14 12:38:00 +0300

    update web site

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/react-developer-Ahmed-Ibrahim-resume.pdf";
+import pdf from "../../Assets/../Assets/Web Developer-Ahmed-Ibrahim.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
