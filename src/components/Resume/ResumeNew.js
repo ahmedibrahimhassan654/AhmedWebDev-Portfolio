dbb77d82257521d5388528c43a58d587d2ab0692
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2024-09-24 10:17:37 +0300

    upload new cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Web Developer-Ahmed-Ibrahim.pdf";
+import pdf from "../../Assets/../Assets/Ahmed Ibrahim - Sr Full Stack Developer.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -18,6 +18,7 @@ function ResumeNew() {
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
+
   return (
     <div>
       <Container fluid className="resume-section">
@@ -33,15 +34,37 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button>
         </Row>
-        <Row className="resume" style={{ marginTop: "20px", marginBottom: "20px", display: "flex", justifyContent: "center" }}>
-  <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-    <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} style={{ maxWidth: "100%", maxHeight: "80vh" }}>
-      {[...Array(numPages)].map((_, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} width={width > 786 ? 1000 : 600} />
-      ))}
-    </Document>
-  </div>
-</Row>
+        <Row
+          className="resume"
+          style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Document
+              file={pdf}
+              onLoadSuccess={onDocumentLoadSuccess}
+              style={{ maxWidth: "100%", maxHeight: "80vh" }}
+            >
+              {[...Array(numPages)].map((_, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  width={width > 786 ? 1000 : 600}
+                />
+              ))}
+            </Document>
+          </div>
+        </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
